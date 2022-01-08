906792bddc2900854173c4a2f4fee8140c629546
YogaAdityaPrabowo-TI <yoga.aditya.ft19@mail.umy.ac.id>
2022-01-09 00:47:24 +0700

    Add project files.

---
+++ WCFService_2Way_20190140053/IClientCallback.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WCFService_2Way_20190140053
+{
+    [ServiceContract]
+    interface IClientCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void pesamKirim(string user, string pesan);
+    }
+}
